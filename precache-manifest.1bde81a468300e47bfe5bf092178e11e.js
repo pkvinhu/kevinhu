e3861aff204c590f7cfd12a9e97d7ea4e9b52556
Kevin Hu <pkvinhu@gmail.com>
2019-01-17 14:46:29 -0500

    Updates

---
+++ precache-manifest.1bde81a468300e47bfe5bf092178e11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8944c19d3b91be48057cd06fb2449a2e",
+    "url": "/kevinhu/static/media/logo-via-logohub.8944c19d.png"
+  },
+  {
+    "revision": "124b90c61e1c4b86bfa6",
+    "url": "/kevinhu/static/js/runtime~main.124b90c6.js"
+  },
+  {
+    "revision": "6daf9d4f30125fd9a0bf",
+    "url": "/kevinhu/static/js/main.6daf9d4f.chunk.js"
+  },
+  {
+    "revision": "188e3b79b3f003ad8d1b",
+    "url": "/kevinhu/static/js/1.188e3b79.chunk.js"
+  },
+  {
+    "revision": "6daf9d4f30125fd9a0bf",
+    "url": "/kevinhu/static/css/main.3c71f92f.chunk.css"
+  },
+  {
+    "revision": "fe3b0f330e5e324e9b752a13a07970bb",
+    "url": "/kevinhu/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kevinhu/precache-manifest.f86468e1489b1d7ed63ea88ddd112e17.js"
+  "/kevinhu/precache-manifest.1bde81a468300e47bfe5bf092178e11e.js"
 );
 
 workbox.clientsClaim();
