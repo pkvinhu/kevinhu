d3b338669f83b80c2e3806ac5328676fa616d797
Kevin Hu <pkvinhu@gmail.com>
2019-01-17 10:14:56 -0500

    Updates

---
+++ precache-manifest.d2acfaa5228f6e02a90dfc42d02094e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kevinhu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "124b90c61e1c4b86bfa6",
+    "url": "/kevinhu/static/js/runtime~main.124b90c6.js"
+  },
+  {
+    "revision": "9680c2c79d7dac190809",
+    "url": "/kevinhu/static/js/main.9680c2c7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/kevinhu/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9680c2c79d7dac190809",
+    "url": "/kevinhu/static/css/main.1c7cccd8.chunk.css"
+  },
+  {
+    "revision": "101992e3a27d37e9f512367f6c646616",
+    "url": "/kevinhu/index.html"
+  }
+];
