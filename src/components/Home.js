a5dcefd270d50cc9bab5cc32607c7ce9a3d3a894
Kevin Hu <pkvinhu@gmail.com>
2019-01-23 12:20:10 -0500

    playing with animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Card, CardContent, CardMedia } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 
 
 class Home extends Component {
+    state = {
+        transform: false
+    }
+
+    handleClick = () => {
+        this.setState({ transform: !this.state.transform })
+    }
+
     render() {
+        const { transform } = this.state;
+        const { handleClick } = this;
         return (
             <div className='home' id='home'>
-                <Card className="bio">
-                    <CardMedia
-                        className="profilePic"
-                        component="img"
-                        image={require('../kevinhu_photo.png')}
+                    <div className="bio">
+                    <img
+                        className={transform ? "profilePic transform" : "profilePic"}
+                        src={require('../kevinhu_photo.png')}
                     />
-                    <CardContent className="cardContent">
-                        <h5>Kevin Hu</h5>
-                        <h5></h5>
-                    </CardContent>
-                </Card>
-                <div className="webContent">
+                    
+                    </div>
+                    <IconButton className="qualities" onClick={handleClick}>account_circle</IconButton>
+                {/*<div className="webContent">
                     <h5>Hi</h5>
                     <h5>information</h5>
-                </div>
+                </div>*/}
             </div>
         )
     }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,9 +6,20 @@ import '../App.css';
 class Nav extends Component {
     render() {
         return (
-            <nav className='App navbar navbar-light'>
-                <img alt='' src={require('../logo-via-logohub.png')} />
-                <div>
+            <nav className='App navbar navbar-light navbar-expand-md'>
+                <img alt='' src={require('../initials.png')} />
+                {/*<button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navBar"
+                    aria-controls="navBar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>*/}
+                <div /*className="collapse navbar-collapse" id="#navBar"*/>
                     <a className='nav-link scroll-link navbar-brand' href='#home'>Home</a>
                     <a className='nav-link scroll-link navbar-brand' href='#about'>About Me</a>
                     <a className='nav-link scroll-link navbar-brand' href='#projects'>Projects</a>
