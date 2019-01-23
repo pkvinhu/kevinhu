7ffd6cb7f66f751d2248ef3be95610061c0fd478
Kevin Hu <pkvinhu@gmail.com>
2019-01-23 13:53:54 -0500

    added animation and edited home section options

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { IconButton } from '@material-ui/core';
+import { Button, Icon, Paper, GridList } from '@material-ui/core';
 
 
 class Home extends Component {
     state = {
-        transform: false
+        transform: false,
+        strengths: false,
+        tech: false,
+        passions: false
     }
 
-    handleClick = () => {
-        this.setState({ transform: !this.state.transform })
+    handleClick = (evt) => {
+        this.setState({ [evt.target.name]: true })
     }
 
     render() {
-        const { transform } = this.state;
+        const { transform, strengths, tech, passions } = this.state;
         const { handleClick } = this;
         return (
             <div className='home' id='home'>
+                    <GridList className="gridContainer" cellHeight={130} cols={1}>
+                        {strengths && <Paper className="tile">Strengths</Paper>}
+                        {tech && <Paper className="tile">Tech</Paper>}
+                        {passions && <Paper className="tile">Passions</Paper>}
+                    </GridList>
                     <div className="bio">
+                    {transform && 
+                        (<div className="insideQualities">
+                            <Button name="strengths" onClick={handleClick}>Strengths</Button>
+                            <Button name="tech" onClick={handleClick}>Technologies</Button>
+                            <Button name="passions" onClick={handleClick}>Passions</Button>
+                        </div>)
+                    }
                     <img
+                        name="transform"
                         className={transform ? "profilePic transform" : "profilePic"}
                         src={require('../kevinhu_photo.png')}
+                        onClick={handleClick}
                     />
                     
                     </div>
-                    <IconButton className="qualities" onClick={handleClick}>account_circle</IconButton>
-                {/*<div className="webContent">
-                    <h5>Hi</h5>
-                    <h5>information</h5>
-                </div>*/}
+                    <div className={transform ? "qualities transform" : "qualities"}>
+                        {!transform && 
+                        (<div><Icon>menu</Icon>
+                        <Icon>arrow_forward_ios</Icon></div>)}
+                    </div>
+
             </div>
         )
     }
