b1eaa930860c1bda34810b3c7593aa5faef325fb
Kevin Hu <pkvinhu@gmail.com>
2019-03-14 14:06:45 -0400

    Updates

---
+++ precache-manifest.4b550fea04e5593227e33d970ba57764.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "82fc5348972726043ee89a930407828e",
+    "url": "/static/media/LitReviews.82fc5348.png"
+  },
+  {
+    "revision": "37c2c7558e4656dfe1e1",
+    "url": "/static/css/main.1b74605c.chunk.css"
+  },
+  {
+    "revision": "09989d658f499068e06a",
+    "url": "/static/js/1.09989d65.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "630467c3c9d849f5acedb5d6f913875a",
+    "url": "/static/media/lightbulb.630467c3.jpg"
+  },
+  {
+    "revision": "e4936486c9f39d72ce98afe5f59c675b",
+    "url": "/static/media/chicago.e4936486.jpg"
+  },
+  {
+    "revision": "ded472805f226fddc44576d3c23d6ccc",
+    "url": "/static/media/brooklyn.ded47280.jpg"
+  },
+  {
+    "revision": "aea24fea2e9ac9db2a828523e4b89c99",
+    "url": "/static/media/github_circle.aea24fea.png"
+  },
+  {
+    "revision": "fbae14d6db1afc47522b75f18b8a9a33",
+    "url": "/static/media/kevinhu_photo.fbae14d6.png"
+  },
+  {
+    "revision": "236428af6241955c116c39b1bd1a1485",
+    "url": "/static/media/faphoto3.236428af.jpg"
+  },
+  {
+    "revision": "2cea6a1851df50588e4477e6db379ca7",
+    "url": "/static/media/RSCapstone.2cea6a18.png"
+  },
+  {
+    "revision": "37c2c7558e4656dfe1e1",
+    "url": "/static/js/main.37c2c755.chunk.js"
+  },
+  {
+    "revision": "b5025ac2da436efe4a1d3803b14cba0c",
+    "url": "/static/media/ManPetals.b5025ac2.png"
+  },
+  {
+    "revision": "0493551dd76d349f20a6da2a4483dd37",
+    "url": "/static/media/Tula.0493551d.png"
+  },
+  {
+    "revision": "b86f6e4f11f963196afab8213944b169",
+    "url": "/static/media/DigInnProjection.b86f6e4f.png"
+  },
+  {
+    "revision": "7cf4c5f6124091d55e142f8ab4f326a4",
+    "url": "/static/media/sneks.7cf4c5f6.png"
+  },
+  {
+    "revision": "df9242f4e04803f170b00991366c5442",
+    "url": "/static/media/navigation.df9242f4.png"
+  },
+  {
+    "revision": "41d1c83abf69f95f58e275900b75e7dc",
+    "url": "/static/media/CultivatingAppetites.41d1c83a.png"
+  },
+  {
+    "revision": "2d307aadf08f49024a7c0ea1351e283a",
+    "url": "/static/media/EngagingPower.2d307aad.png"
+  },
+  {
+    "revision": "ac46886b15792eb5e8339142bbe2b1ea",
+    "url": "/static/media/NamingViolence.ac46886b.png"
+  },
+  {
+    "revision": "b8809e5d2aceffb86a318f82a77f4dea",
+    "url": "/static/media/ExcavatingMemory.b8809e5d.png"
+  },
+  {
+    "revision": "9a1efd3b3617e88e328e3369684f2acf",
+    "url": "/index.html"
+  }
+];
